9731b43823632c287833805de7707e935a73a381
sumon <sumon914559yahoo.com>
2021-06-22 13:40:47 +0600

    initial commit done

---
+++ ReceiverApp/EventSenderTut.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Common;
+using MassTransit;
+
+namespace ReceiverApp
+{
+    public class EventSenderTut : IConsumer<Product>
+    {
+        public async Task Consume(ConsumeContext<Product> context)
+        {
+            var tt = context.Message;
+        }
+    }
+}
